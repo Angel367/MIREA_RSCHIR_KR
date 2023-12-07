b410a87e7e5dfb7270ef80f321c06d3eab78d063
Egor Volkov <egor.volkov20038@gmail.com>
2023-12-07 22:37:02 +0300

    base

---
+++ MIREA_RSCHIR_KR/cosmetic/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+# urlpatterns = [
+#     path('volkov/<username>', views.hello_world_page, name='hello_world_page'),
+#     path('volkov/', views.hello_world_page_anonymous, name='hello_world_page_anonymous')
+# ]
