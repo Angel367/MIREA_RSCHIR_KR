e01b7dc260d0f1355f98fa94c6c2b04408eac7b4
Egor Volkov <egor.volkov20038@gmail.com>
2023-12-08 21:51:20 +0300

    added js

--- MIREA_RSCHIR_KR/cosmetic/static/animation.js
+++ MIREA_RSCHIR_KR/cosmetic/static/animation.js
@@ -2,4 +2,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     var header = document.querySelector('header');
     header.classList.add('visible');
+    console.log("header - visible")
 });
